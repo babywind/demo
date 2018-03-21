e83d444c0c9b3555e75fa2b167c30265e71e2d68
babywind <32251467+babywind@users.noreply.github.com>
2018-03-21 21:13:22 +0800

    Initial commit

---
+++ src/main/java/com/xy/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.xy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
